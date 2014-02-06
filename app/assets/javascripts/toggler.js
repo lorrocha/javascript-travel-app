f2fb40031e208a90c881fee442126da5e8563467
Lorry Rocha <sorai120@yahoo.com>
2014-02-06 14:11:32 -0500

    Refractored code in toggler.js to reflect object model, instead of one giant function

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,18 +1,42 @@
-function init() {
-  var $toggleLinks = $('.detail_toggle');
-  $toggleLinks.click(function(ev) {
-    var $detail = $(this).siblings('.detail');
-    $detail.toggleClass('hidden');
-    if($detail.hasClass('hidden')) {
-      $(this).text('Show Details');
-    } else {
-      $(this).text('Hide Details');
-    }
-    ev.preventDefault();
-  });
-
-}
+var toggler = {
+  linkSelector: ".detail_toggle",
+  detailSelector: ".detail",
+  hiddenClass: "hidden",
+  hideText: "Hide Details",
+  showText: "Show Details",
+  init: function() {
+    var self = this;
+    $(this.linkSelector).on("click", function(event) {
+      self.toggleOnClick(event)
+    });
+  },
+  toggleOnClick: function(event) {
+    this.$link = $(event.target);
+    this.$link.text(this.isDetailHidden() ? this.hideText : this.showText); this.detailElement().toggleClass(this.hiddenClass); event.preventDefault();
+  },
+  detailElement: function() {
+    return this.$link.parent().find(this.detailSelector);
+  },
+  isDetailHidden: function() {
+    return this.detailElement().hasClass(this.hiddenClass);
+  }
+};
 
 $(function() {
-  init();
+  toggler.init();
 });
+
+// function init() {
+//   var $toggleLinks = $('.detail_toggle');
+//   $toggleLinks.click(function(ev) {
+//     var $detail = $(this).siblings('.detail');
+//     $detail.toggleClass('hidden');
+//     if($detail.hasClass('hidden')) {
+//       $(this).text('Show Details');
+//     } else {
+//       $(this).text('Hide Details');
+//     }
+//     ev.preventDefault();
+//   });
+
+// }
