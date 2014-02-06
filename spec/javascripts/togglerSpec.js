3b7643f8ddaf27b61a95c62237ac1799ae927a7d
Lorry Rocha <sorai120@yahoo.com>
2014-02-06 14:13:01 -0500

    Woops! Had to update the test spec to reflect changes in the toggler.js file. Tests are working now.

--- spec/javascripts/togglerSpec.js
+++ spec/javascripts/togglerSpec.js
@@ -2,7 +2,7 @@ describe("Trip detail toggler", function() {
   describe("clicking a show description link", function() {
     beforeEach(function() {
       loadFixtures("one_index_trip.html");
-      init();
+      toggler.init();
       $(".detail_toggle").click();
     });
 
